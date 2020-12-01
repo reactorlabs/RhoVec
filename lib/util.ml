dfe24ea1b30313794c4451793a61b8d3b1366554
Ming-Ho Yee <mh@mhyee.com>
2020-12-01 17:36:59 -0500

    REPL can handle some directives

--- lib/util.ml
+++ lib/util.ml
@@ -27,3 +27,23 @@ module Option = struct
     | None -> default
     | Some x -> f x
 end
+
+module String = struct
+  include String
+
+  let prefix ~pre s =
+    let len_p = String.length pre in
+    if len_p > String.length s then false
+    else
+      let rec aux i =
+        if i = len_p then true
+        else if String.unsafe_get pre i <> String.unsafe_get s i then false
+        else aux (i + 1) in
+      aux 0
+
+  let chop_prefix ~pre s =
+    if prefix ~pre s then
+      let len_p = String.length pre in
+      Some (String.sub s len_p (String.length s - len_p))
+    else None
+end
--- src/main.ml
+++ src/main.ml
@@ -1,44 +1,57 @@
 open Lib
+open Util
 
 (* TODO: git push hook for ocamlformat *)
+(* TODO: code coverage in CI? *)
 
-(* TODO: two modes
-  - help flag
-  - read file flag
-      - parse and eval
-      - print final result
-        - no print command so only the final result gets printed
-      - maybe add a print command for output?
-      - mode to deparse to R
-  - interactive mode / repl (no flags)
-      - directives:
-          - help
-          - to R
-          - quit
+(* TODO:
+    - file mode: read and execute RhoVec file, or translate it to R
+    - also need a flag for help
 *)
 
-(* TODO: tests for repl? *)
+let repl_help () =
+  Stdlib.print_endline "Enter a RhoVec expression to be evaluated." ;
+  Stdlib.print_endline "Type '#h' to print this message." ;
+  Stdlib.print_endline "Type '#r [exp]' to translate exp to R syntax." ;
+  Stdlib.print_endline "Type '#q' or CTRL+D to quit."
 
 let run_once env =
+  let handle_directive input =
+    if input = "#h" then repl_help ()
+    else if String.prefix ~pre:"#r " input then
+      let rv_input = Option.get @@ String.chop_prefix ~pre:"#r " input in
+      try
+        let exp = Parse.parse rv_input in
+        Stdlib.print_endline @@ Deparse.to_r exp
+      with Parse.Parse_error msg -> Printf.printf "Error%s\n" msg
+    else if input = "#q" then raise End_of_file
+    else Printf.printf "Error: unknown directive\n" in
+
+  let run input =
+    try
+      let exp = Parse.parse input in
+      let env', result = Eval.eval env exp in
+      Stdlib.print_endline @@ Expr.show_val result ;
+      (* return the new environment *)
+      env'
+    with e ->
+      ( match e with
+      | Parse.Parse_error msg -> Printf.printf "Error%s\n" msg
+      | Eval.Type_error { expected; received } ->
+          Printf.printf "Error: expected type %s but received %s\n" (Expr.show_type expected)
+            (Expr.show_type received)
+      | e ->
+          let msg = Eval.excptn_to_string e in
+          Printf.printf "Error: %s\n" msg ) ;
+      (* return the old environment *)
+      env in
+
   Stdlib.print_string "> " ;
   let input = Stdlib.read_line () in
-  try
-    let exp = Parse.parse input in
-    let env', result = Eval.eval env exp in
-    Stdlib.print_endline @@ Expr.show_val result ;
-    (* return the new environment *)
-    env'
-  with e ->
-    ( match e with
-    | Parse.Parse_error msg -> Printf.eprintf "Error%s\n" msg
-    | Eval.Type_error { expected; received } ->
-        Printf.eprintf "Error: expected type %s but received %s\n" (Expr.show_type expected)
-          (Expr.show_type received)
-    | e ->
-        let msg = Eval.excptn_to_string e in
-        Printf.eprintf "Error: %s\n" msg ) ;
-    (* return the old environment *)
-    env
+  if String.prefix ~pre:"#" input then (
+    handle_directive input ;
+    env )
+  else run input
 
 let repl () =
   let rec loop env =
@@ -46,7 +59,8 @@ let repl () =
     (loop [@tailcall]) env' in
 
   let env = Expr.Env.empty in
-  Stdlib.print_endline "Welcome to the RhoVec REPL. CTRL+D exits." ;
+  Stdlib.print_endline "Welcome to the RhoVec REPL.\n" ;
+  repl_help () ;
   try loop env with End_of_file -> Stdlib.print_endline "\nGoodbye!"
 
 let () = repl ()
