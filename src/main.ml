30f133e5e704e6bf4ee890ffe529eaa4ba099197
Ming-Ho Yee <mh@mhyee.com>
2020-12-01 16:06:33 -0500

    Make the Read-Eval-Print Loop

--- src/main.ml
+++ src/main.ml
@@ -1,39 +1,52 @@
 open Lib
 
+(* TODO: git push hook for ocamlformat *)
+
 (* TODO: two modes
-  - read file
+  - help flag
+  - read file flag
       - parse and eval
       - print final result
         - no print command so only the final result gets printed
       - maybe add a print command for output?
       - mode to deparse to R
-  - interactive mode / repl
-      - read a line of input, eval and print it
-      - add some mode to deparse to R
+  - interactive mode / repl (no flags)
+      - directives:
+          - help
+          - to R
+          - quit
 *)
 
-let () =
-  let env = Expr.Env.empty in
+(* TODO: tests for repl? *)
+
+let run_once env =
+  Stdlib.print_string "> " ;
+  let input = Stdlib.read_line () in
   try
-    let line = Stdlib.read_line () in
-    let exp = Parse.parse line in
-    let _, result = Eval.eval env exp in
-    print_endline @@ Expr.show_val result
-  with
-  | Parse.Parse_error msg -> Printf.printf "Parse error%s\n" msg
-  | Eval.Type_error { expected; received } ->
-      Printf.printf "Eval error: expected type %s but received %s\n" (Expr.show_type expected)
-        (Expr.show_type received)
-  | e ->
-      let msg = Eval.excptn_to_string e in
-      Printf.printf "Eval error: %s\n" msg
+    let exp = Parse.parse input in
+    let env', result = Eval.eval env exp in
+    Stdlib.print_endline @@ Expr.show_val result ;
+    (* return the new environment *)
+    env'
+  with e ->
+    ( match e with
+    | Parse.Parse_error msg -> Printf.eprintf "Error%s\n" msg
+    | Eval.Type_error { expected; received } ->
+        Printf.eprintf "Error: expected type %s but received %s\n" (Expr.show_type expected)
+          (Expr.show_type received)
+    | e ->
+        let msg = Eval.excptn_to_string e in
+        Printf.eprintf "Error: %s\n" msg ) ;
+    (* return the old environment *)
+    env
 
-(*
-  let str = "Combine(1, -Combine(-2, -3), 4, 5, \n NA_i);" in
-  let expr = Parse.parse str in
-  let res = Eval.run expr in
-  Format.printf "Input:\n%S\n\n" str ;
-  Format.printf "Parsed expression:\n%s\n\n" (Expr.show_expression expr) ;
-  Format.printf "Evaluated result:\n%s\n\n" (Expr.show_val res) ;
-  print_endline "Nothing else for now, maybe you meant to run the tests?"
-*)
+let repl () =
+  let rec loop env =
+    let env' = run_once env in
+    (loop [@tailcall]) env' in
+
+  let env = Expr.Env.empty in
+  Stdlib.print_endline "Welcome to the RhoVec REPL. CTRL+D exits." ;
+  try loop env with End_of_file -> Stdlib.print_endline "\nGoodbye!"
+
+let () = repl ()
